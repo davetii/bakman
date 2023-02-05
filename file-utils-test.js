39ecdaba9612f5e68deff8d29722382e779d6da4
Dave Turner <davetii@gmail.com>
2023-02-05 00:15:34 -0500

    adding tests

--- file-utils-test.js
+++ file-utils-test.js
@@ -5,9 +5,27 @@ import {sep} from 'path';
 
 describe('ensure file-utils function as expected', ()=> {
     context('ensure filesize returns expected', () => {
-        it ('should return fileisize', async () => {
+        it ('should return correct filesize', async () => {
             const actualFileSize = await fu.fileSize('test-content' + sep + 'lorem-epsom-do-not-change.txt');
             assert.equal(actualFileSize, 3862);
-        })
+        });
     })
+
+    context('ensure readPath returns expected', () => {
+        it ('should return correct count of items', async () => {
+            const items = await fu.readPath('test-content' + sep + 'do-not-change');            
+            console.log(items);
+            assert.equal(items.length, 3);
+        });
+    })
+
+
+    context('ensure dirSize returns expected', () => {
+        it ('should return correct dirSize', async () => {
+            const items = await fu.readPath('test-content' + sep + 'do-not-change');            
+            const actualDirSize = await fu.dirSize(items);
+            assert.equal(actualDirSize, 100518);
+        });
+    })
+
 })
