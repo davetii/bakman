e2743f511cbd30e80f7d2219b7b1a834e5f06147
Dave Turner <davetii@gmail.com>
2023-04-10 01:08:19 -0400

    bug fix in tmp test

--- tmp-test.js
+++ tmp-test.js
@@ -1,8 +1,6 @@
 import * as assert from 'assert';
 import { expect } from 'chai';
 import {sep, resolve} from 'path';
-import * as fu from './file-utils.js';
-import * as su from './string-utils.js';
 
 describe('ensure file-utils function as expected', ()=> {
 
