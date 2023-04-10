7275abdf0d030da21a1f9475bb54617bec21a4ce
Dave Turner <davetii@gmail.com>
2023-04-10 01:05:35 -0400

    adding utility for strings and lists

--- bakman-utils-test.js
+++ bakman-utils-test.js
@@ -6,6 +6,24 @@ describe('ensure String-utils function as expected', ()=> {
     context('ensure containsString works', () => {
         it ('should return expected', async () => {
             assert.equal(su.containsString(['andy', 'bob', 'caldwell'], 'bob'), true);
+        });
+
+        it ('should handle empty', async () => {
+            assert.equal(su.containsString([], 'bob'), false);
+        });
+
+        it ('should handle not a list empty expected', async () => {
+            assert.equal(su.containsString({}, 'bob'), false);
+        });
+
+        it ('should handle null', async () => {
+            assert.equal(su.containsString(null, 'bob'), false);
+        });
+    })
+
+    context('ensure diffList works', () => {
+        it ('should return expected', async () => {
+            assert.equal(su.containsString(['andy', 'bob', 'caldwell'], 'bob'), true);
             assert.equal(su.containsString([], 'bob'), false);
             assert.equal(su.containsString({}, 'bob'), false);
             assert.equal(su.containsString(null, 'bob'), false);
--- bakman-utils.js
+++ bakman-utils.js
@@ -7,3 +7,32 @@ export function containsString(list, s) {
     }
     return false;
 }
+
+export function isListEmpty(list) {
+    if (list == null || list == undefined || list.length == 0) { return true; }
+    return false;
+}
+
+export async function diffList(list1, list2) {
+    if(isListEmpty(list1) || isListEmpty(list2)) 
+    return [];
+    const items = [];
+    let i=0;
+    let x=0;
+
+    for(i=0;i < list1.length; i++ ) 
+    {
+        for (x=0; x < list2.length; x++) {
+            if(list1[i] == list2[x]) { 
+                items.push(list2[x]); 
+                break;
+            }
+        }
+    }
+    return items;
+    
+
+
+
+
+}
